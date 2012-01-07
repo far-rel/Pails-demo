333ffef12562dffef3783f8fff7bd9ce7d14d7ed
Paweł Tarczykowski <farrel1@wp.pl>
2012-01-07 20:27:51 +0100

    Session test

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -3,7 +3,11 @@ from Pails.core.controllers import BaseController
 class UserController(BaseController):
     
     def show(self):
-        self.render(params = { 'echo' : str(self._params) + ' Cookie: ' + str(self.cookies['user'] )})
+        if self.session['count']:
+            self.session['count'] = int(self.session['count']) + 1
+        else:
+            self.session['count'] = 1
+        self.render(params = { 'echo' : str(self._params) + ' Cookie: ' + str(self.cookies['user'] + 'Count: ' + str(self.session['count'] ))})
 
     def new(self):
         self.redirect(self.path.account())
